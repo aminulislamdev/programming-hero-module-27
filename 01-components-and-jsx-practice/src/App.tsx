b790405caf508006a20c9b3d52613a8760cf805d
Aminul islam <aminulislam424842@gmail.com>
2026-09-03 18:11:34 +0600

    feat: add information component with JSX

--- 01-components-and-jsx-practice/src/App.tsx
+++ 01-components-and-jsx-practice/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import {Welcome} from './component/welcome'
+import { Information } from './component/UserCard'
 
 function App() {
 
   return (
     <>
       <Welcome ></Welcome>
+      <Information></Information>
     </>
   )
 }
---
+++ 01-components-and-jsx-practice/src/component/UserCard.tsx
@@ -0,0 +1,9 @@
+export function Information () {
+  return (
+    <>
+      <p>Age : 21</p>
+      <p>Location : Rajshahi,Bangladesh</p>
+      <p>Skill : ReactJS</p>
+    </>
+  )
+}
