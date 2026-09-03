72739763561685b48da584d485a3e732442bbdfd
Aminul islam <aminulislam424842@gmail.com>
2026-09-03 18:00:44 +0600

    feat: add first React component with JSX

--- 01-components-and-jsx-practice/src/App.tsx
+++ 01-components-and-jsx-practice/src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css'
+import {Welcome} from './component/welcome'
 
 function App() {
 
   return (
-    <></>
+    <>
+      <Welcome ></Welcome>
+    </>
   )
 }
 
---
+++ 01-components-and-jsx-practice/src/component/welcome.tsx
@@ -0,0 +1,8 @@
+export function Welcome() {
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <h2>Name: Aminul Islam Mahi</h2>
+    </div>
+  )
+}
