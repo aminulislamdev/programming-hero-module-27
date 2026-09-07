673666cf215f7cf8d93eadbb91fc9defe550f0ce
Aminul islam <aminulislam424842@gmail.com>
2026-09-07 17:46:20 +0600

    Add Header Profile Footer components with exports and imports

--- 03-components-and-props/src/App.tsx
+++ 03-components-and-props/src/App.tsx
@@ -1,4 +1,7 @@
 import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Profile from './components/Profile'
 import Status from './components/Status'
 import Student from './components/Student'
 import User from './components/User'
@@ -8,6 +11,10 @@ function App() {
     <User name="Aminul Islam" age={21} />
     <Student name="Aminul Islam" department='Computer Science & Technology' semester={5}/>
     <Status name="Aminul" isActive={true}/>
+
+    <Header></Header>
+    <Profile></Profile>
+    <Footer></Footer>
   </>
 }
 
---
+++ 03-components-and-props/src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div>
+      <h2>MY Website</h2>
+    </div>
+  )
+}
